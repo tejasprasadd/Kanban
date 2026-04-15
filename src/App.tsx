e6e68602995017b0642b05254bc05cb79967a541
tejasprasadd <tejasprasad43@gmail.com>
2026-04-15 14:34:39 +0530

    Fixed dependencies and removed unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import "./index.css";
 
 export function App() {
-  return (
- <>
-  <h1>Kanban</h1>
-  </>
-  );
+  return <h1 className="text-2xl font-bold text-center">Kanban</h1>;
 }
 
 export default App;
